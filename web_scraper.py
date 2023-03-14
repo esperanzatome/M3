4743b0702850d073d3a9e22a383c3efa6694cb0f
Esperanza <espetomeiglesias@gmail.com>
2023-03-14 21:28:48 +0100

    'M3'

---
+++ web_scraper.py
@@ -0,0 +1,17 @@
+import requests
+import pprint
+import inflection
+from bs4 import BeautifulSoup
+r = requests.get('http://www.dailysmarty.com/topics/python')
+pagina= BeautifulSoup(r.text,features="html5lib")
+posts= pagina.find_all('h2')
+lista_posts=list(posts)[:]
+def web_scraper():
+  for post in lista_posts:
+    post.get_text()
+    print(post.get_text())
+web_scraper()    
+    
+
+
+  
