4309929622c09be8e0967f6618aa668b31931e4a
esperanzatome <125577327+esperanzatome@users.noreply.github.com>
2023-03-14 22:42:04 +0100

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 r = requests.get('http://www.dailysmarty.com/topics/python')
-pagina= BeautifulSoup(r.text,features="html")
+pagina= BeautifulSoup(r.text,features="html.parser")
 posts= pagina.find_all('h2')
 lista_posts=list(posts)
 def web_scraper():
